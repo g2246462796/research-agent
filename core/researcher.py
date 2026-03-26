e186ac19d6bc72eafd478fe1352b592ae9f8da78
Orion <gch2246462796@163.com>
2026-03-26 19:06:44 +0800

    重新整理项目结构

--- core/researcher.py
+++
@@ -1,9 +0,0 @@
-class Researcher:
-    def __init__(self, llm):
-        self.llm = llm
-
-    def shallow_search(self, query):
-        return "快速搜索结果"
-
-    def deep_research(self, query):
-        return "深度研究报告"
